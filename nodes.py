55c8401f5e57d28e75e645330a58839ceaf12b4b
Draken <rootdraken@gmail.com>
2025-05-03 19:01:25 +0200

    F1 fix, force first frame to be input (could be opt)

--- nodes.py
+++ nodes.py
@@ -482,8 +482,11 @@ class FramePackSampler:
         num_frames = latent_window_size * 4 - 3
 
         history_latents = torch.zeros(size=(1, 16, 1 + 2 + 16, H, W), dtype=torch.float32).cpu()
-       
-        total_generated_latent_frames = 0
+        if forward_generation:
+            history_latents = torch.cat([history_latents, start_latent.to(history_latents)], dim=2)
+            total_generated_latent_frames = 1
+        else:
+            total_generated_latent_frames = 0
 
         if forward_generation:
             latent_paddings_list = list(range(total_latent_sections))  # forward
@@ -549,13 +552,13 @@ class FramePackSampler:
                 clean_latent_indices = torch.cat([clean_latent_indices_pre, clean_latent_indices_post], dim=1)
 
 
-            clean_latents_pre = start_latent.to(history_latents)
-
             if forward_generation:
+                
                 clean_latents_4x, clean_latents_2x, clean_latents_1x = history_latents[:, :, -sum([16, 2, 1]):, :, :].split([16, 2, 1], dim=2)
                 clean_latents = torch.cat([start_latent.to(history_latents), clean_latents_1x], dim=2)
 
             else:
+                clean_latents_pre = start_latent.to(history_latents)
                 clean_latents_post, clean_latents_2x, clean_latents_4x = history_latents[:, :, :1 + 2 + 16, :, :].split([1, 2, 16], dim=2)
                 clean_latents = torch.cat([clean_latents_pre, clean_latents_post], dim=2)
 
